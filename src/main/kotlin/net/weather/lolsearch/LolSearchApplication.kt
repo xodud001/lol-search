9a3b81328b4322bde4b527fdd9c062f914a40c38
김태영 <freeptk1@gmail.com>
2023-01-18 22:18:39 +0900

    init: 프로젝트 초기화

---
+++ src/main/kotlin/net/weather/lolsearch/LolSearchApplication.kt
@@ -0,0 +1,11 @@
+package net.weather.lolsearch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LolSearchApplication
+
+fun main(args: Array<String>) {
+	runApplication<LolSearchApplication>(*args)
+}
---
+++ src/test/kotlin/net/weather/lolsearch/LolSearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.weather.lolsearch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LolSearchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
