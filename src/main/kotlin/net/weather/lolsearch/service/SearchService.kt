571906dcbd33774419ecc7a469ffc3035a17098a
김태영 <freeptk1@gmail.com>
2023-01-24 22:37:46 +0900

    feat: Match 정보 조회하는 로직 비동기로 변경

--- src/main/kotlin/net/weather/lolsearch/service/SearchService.kt
+++ src/main/kotlin/net/weather/lolsearch/service/SearchService.kt
@@ -3,17 +3,23 @@ package net.weather.lolsearch.service
 import net.weather.lolsearch.riot.api.RiotApi
 import net.weather.lolsearch.riot.dto.MatchDto
 import org.springframework.stereotype.Service
+import java.util.concurrent.CompletableFuture
 
 @Service
 class SearchService(
     val api: RiotApi
 ) {
+
     fun findMatches(nickname: String): SearchDto {
         val (_, _, puuid) = api.getSummonerByNickname(nickname);
 
         val matchIds = api.getMatchIds(puuid);
 
-        val matches = matchIds.map { matchId -> api.getMatch(matchId) };
+        val matches = matchIds.map { matchId ->
+            Thread.sleep(40) // 20 request every 1 seconds 제약
+            CompletableFuture.supplyAsync { api.getMatch(matchId)
+            }}
+            .map { future -> future.get() }
 
         return SearchDto(matches, puuid);
     }
