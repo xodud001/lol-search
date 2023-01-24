ec6165798ac34da05262fad8ffa112811b95a940
김태영 <freeptk1@gmail.com>
2023-01-24 12:04:39 +0900

    feat: riot api 응답의 http status에 따른 예외처리

--- src/main/kotlin/net/weather/lolsearch/riot/api/RiotApiImpl.kt
+++ src/main/kotlin/net/weather/lolsearch/riot/api/RiotApiImpl.kt
@@ -3,6 +3,8 @@ package net.weather.lolsearch.riot.api
 import net.weather.lolsearch.riot.dto.MatchDto
 import net.weather.lolsearch.riot.exception.SummonerNotFoundException
 import net.weather.lolsearch.riot.dto.SummonerDto
+import net.weather.lolsearch.riot.exception.MatchNotFoundException
+import net.weather.lolsearch.riot.exception.RateLimitException
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.HttpStatus
@@ -16,7 +18,7 @@ import java.net.URI
 @Component
 class RiotApiImpl: RiotApi {
 
-    private val apiKey: String = "RGAPI-020559eb-87cf-4220-aa77-763dc0981419";
+    private val apiKey: String = "RGAPI-11d08a0e-02e1-45f0-87ab-796def0f0f9b";
     private val client: WebClient = WebClient.builder().build();
 
     override fun getMatchIds(puuid: String): List<String> {
@@ -24,6 +26,14 @@ class RiotApiImpl: RiotApi {
             .uri(URI.create("https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/$puuid/ids?start=0&count=20"))
             .header("X-Riot-Token", apiKey)
             .retrieve()
+            .onStatus({ obj: HttpStatusCode -> obj == HttpStatus.NOT_FOUND })
+            { throw MatchNotFoundException("존재하지 않는 puuid 입니다. puuid=[$puuid]") }
+            .onStatus({ code: HttpStatusCode -> code == HttpStatus.TOO_MANY_REQUESTS})
+            { throw RateLimitException("요청이 너무 많습니다.")}
+            .onStatus({ obj: HttpStatusCode -> obj.is4xxClientError })
+            { throw IllegalArgumentException("요청이 잘못되었습니다.") }
+            .onStatus({ obj: HttpStatusCode -> obj.is5xxServerError })
+            { throw IllegalStateException("Riot 서버에 문제가 발생했습니다.") }
             .bodyToMono(object : ParameterizedTypeReference<List<String>>(){})
             .block()
 
@@ -35,6 +45,14 @@ class RiotApiImpl: RiotApi {
             .uri(URI.create("https://asia.api.riotgames.com/lol/match/v5/matches/$matchId"))
             .header("X-Riot-Token", apiKey)
             .retrieve()
+            .onStatus({ obj: HttpStatusCode -> obj == HttpStatus.NOT_FOUND })
+            { throw MatchNotFoundException("매치가 존재하지 않습니다. matchId=[$matchId]") }
+            .onStatus({ code: HttpStatusCode -> code == HttpStatus.TOO_MANY_REQUESTS})
+            { throw RateLimitException("요청이 너무 많습니다.")}
+            .onStatus({ obj: HttpStatusCode -> obj.is4xxClientError })
+            { throw IllegalArgumentException("요청이 잘못되었습니다.") }
+            .onStatus({ obj: HttpStatusCode -> obj.is5xxServerError })
+            { throw IllegalStateException("Riot 서버에 문제가 발생했습니다.") }
             .bodyToMono(MatchDto::class.java)
             .block()
 
@@ -51,8 +69,10 @@ class RiotApiImpl: RiotApi {
             .uri(URI.create("https://kr.api.riotgames.com$path"))
             .header("X-Riot-Token", apiKey)
             .retrieve()
-            .onStatus({ obj: HttpStatusCode? -> obj == HttpStatus.NOT_FOUND })
+            .onStatus({ obj: HttpStatusCode -> obj == HttpStatus.NOT_FOUND })
             { throw SummonerNotFoundException("소환사가 존재하지 않습니다. $path") }
+            .onStatus({ code: HttpStatusCode -> code == HttpStatus.TOO_MANY_REQUESTS})
+            { throw RateLimitException("요청이 너무 많습니다.")}
             .onStatus({ obj: HttpStatusCode -> obj.is4xxClientError })
             { throw IllegalArgumentException("요청이 잘못되었습니다.") }
             .onStatus({ obj: HttpStatusCode -> obj.is5xxServerError })
---
+++ src/main/kotlin/net/weather/lolsearch/riot/exception/MatchNotFoundException.kt
@@ -0,0 +1,4 @@
+package net.weather.lolsearch.riot.exception
+
+class MatchNotFoundException(message: String?) : RuntimeException(message) {
+}
